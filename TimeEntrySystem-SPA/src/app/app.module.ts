33c865c3bb3fdb577b2d08ad5cb34b1c29a09d7d
DESKTOP-1L2LVO0\eddie <eddie97g@gmail.com>
2019-02-16 14:24:10 -0600

    One hardcoded employee on front-end

--- TimeEntrySystem-SPA/src/app/app.module.ts
+++ TimeEntrySystem-SPA/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { HomeComponent } from './home/home.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      EmployeeComponent
+   ],
+   imports: [
+      BrowserModule,
+      MatListModule,
+      BrowserAnimationsModule,
+      FlexLayoutModule
+   ],
+   providers: [
+      JwtHelperService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
--- TimeEntrySystem.API/Startup.cs
+++ TimeEntrySystem.API/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TimeEntrySystem.API.Data;
 
 namespace TimeEntrySystem.API
 {
@@ -26,6 +28,8 @@ namespace TimeEntrySystem.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,10 +42,11 @@ namespace TimeEntrySystem.API
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
